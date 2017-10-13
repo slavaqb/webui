ba458d7587bf1ac30be97d626d8fe116e1ebf3b3
slavaqb <32763819+slavaqb@users.noreply.github.com>
2017-10-13 09:43:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import os, time
+import os, time, threading, datetime
 from selenium import webdriver
+
+class Speaker():
+    def __init__(self, interval):
+        self.interval = interval
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        while (True):
+            print('{dt}'.format(dt=datetime.datetime.now()))
+            sleep(self.interval)
 
 def main():
     print('def main()')
@@ -7,7 +19,9 @@ def main():
     driver.get("http://rolling-horse.000webhostapp.com/")
     html_source = driver.page_source
     print(html_source)
+    Speaker(30)
     time.sleep(60*30)
     driver.close()
+    print('Ok')
 if __name__ == '__main__':
     main()
