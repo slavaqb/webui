cd373f3e2e305302bfd06acd1872ce73ba169f5d
slavaqb <32763819+slavaqb@users.noreply.github.com>
2017-10-13 09:20:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+from selenium import webdriver
 
 def main():
     print('def main()')
-
+    driver = webdriver.Firefox()
+    driver.get("http://www.python.org")
+    html_source = driver.page_source
+    print(html_source)
+    driver.close()
 if __name__ == '__main__':
     main()
